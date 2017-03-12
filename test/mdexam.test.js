4d3433f8ebc332bba28a6063c522b9e8df2ee6ad
胡小根 <hxgqh@126.com>
2017-03-13 07:00:49 +0800

    coding

--- test/mdexam.test.js
+++ test/mdexam.test.js
@@ -8,14 +8,11 @@ const mdexam = require('../lib/mdexam');
 
 let md = mdexam();
 
-
-// 测试切分markdown文件
-test('Test split exam markdown to question markdown', () => {
+test('测试将markdown文档切换成多个问题', () => {
   expect(1 + 2).toBe(3);
 });
 
-// 测试将选择题转换成JSON
-test('Convert multiple choice markdown question to json', () => {
+test('测试将选择题转换成JSON', () => {
   var mdq = "\
 ## [选择题] 题目内容\
 \n\
@@ -48,8 +45,7 @@ test('Convert multiple choice markdown question to json', () => {
   expect(qj.answers.indexOf('选项2')).toBeGreaterThanOrEqual(0);
 });
 
-// 测试将多选题转换成JSON
-test('Convert fill-in markdown question to json', () => {
+test('测试将多选题转换成JSON', () => {
   var mdq = "## [填空题] 题目内容\n\
 #### [标签]\n\
   * 标签11\n\
@@ -69,8 +65,7 @@ test('Convert fill-in markdown question to json', () => {
   expect(qj.checker[0]['answer-regex']).toBe('/\\*\\s\\[answer-regex\\]\\s{0,}(\\S+)/g');
 });
 
-// 测试解析整个markdown文件
-test('Convert markdown to json', () => {
+test('测试解析整个markdown文件', () => {
   fs.readFile(__dirname + '/exam.md', {flag: 'r+', encoding: 'utf8'}, function (err, markdown) {
     if(err) {
       console.error(err);
